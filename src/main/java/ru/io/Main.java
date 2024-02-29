8e9b2af17512cff11cc3b2a8f904053b31a3e6c8
il <>
2024-02-29 21:08:56 +0300

    change format string

--- src/main/java/ru/io/Main.java
+++ src/main/java/ru/io/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("index = " + index);
+            System.out.printf("index = " + index);
         }
     }
 }
