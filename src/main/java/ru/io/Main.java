1f1f659e0731d6fc59a9e3d0bd56971faf328d6f
il <>
2024-02-29 20:09:55 +0300

    add change

--- src/main/java/ru/io/Main.java
+++ src/main/java/ru/io/Main.java
@@ -9,12 +9,11 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int index = 1; index <= 5; index++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("index = " + index);
         }
-
     }
 }
